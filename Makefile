7ba5c511f471f407c74df5b32f1a5ed46c734380
patricioadolfo.castillocalderon <you@example.com>
2022-06-18 14:19:22 +0200

    first commint

---
+++ Makefile
@@ -0,0 +1,71 @@
+FLAGS =  -std=c11  -D_DEFAULT_SOURCE  -Werror -lm $(shell pkg-config --cflags --libs check) -L/usr/local/lib -lssl -lcrypto
+SERVER_IP = 127.0.0.1
+PORT = 8069
+SERVER_PARAM = $(PORT) 1 keys/server.crt keys/server_rsa_private.pem.unsecure
+CLIENT_PARAM = $(SERVER_IP) $(PORT) keys/client.crt keys/client_rsa_private.pem.unsecure
+server: 
+	@echo -e '\n*******************************'
+	@echo -e '*** Compiling for UNIT TEST ***'
+	@echo -e '*******************************'
+	@mkdir -p build
+	@gcc  source/server_cert.c -o build/server_test $(FLAGS)
+	@echo -e '\n*************************'
+	@echo -e '*** Running UNIT TEST ***'
+	@echo -e '*************************'
+	@sudo ./build/server_test $(SERVER_PARAM)
+
+client: 
+	@echo -e '\n*******************************'
+	@echo -e '*** Compiling for UNIT TEST ***'
+	@echo -e '*******************************'
+	@mkdir -p build
+	@gcc  source/client_cert.c -o build/client_test $(FLAGS)
+	@echo -e '\n*************************'
+	@echo -e '*** Running UNIT TEST ***'
+	@echo -e '*************************'
+	@sudo ./build/client_test $(CLIENT_PARAM) 
+
+
+server_example: server_pigeon/server_tls.c 
+	@echo -e '\n*******************************'
+	@echo -e '*** Compiling for UNIT TEST ***'
+	@echo -e '*******************************'
+	@mkdir -p build
+	@gcc  server_pigeon/server_tls.c -o build/server_test $(FLAGS)
+	@echo -e '\n*************************'
+	@echo -e '*** Running UNIT TEST ***'
+	@echo -e '*************************'
+	@sudo ./build/server_test $(PORT)
+
+client_example: 
+	@echo -e '\n*******************************'
+	@echo -e '*** Compiling for UNIT TEST ***'
+	@echo -e '*******************************'
+	@mkdir -p build
+	@gcc  client_pigeon/client_tls.c -o build/client_test $(FLAGS)
+	@echo -e '\n*************************'
+	@echo -e '*** Running UNIT TEST ***'
+	@echo -e '*************************'
+	@sudo ./build/client_test $(SERVER_IP) $(PORT)
+
+debug: main.c sbuffer.c
+	@echo -e '\n*******************************'
+	@echo -e '*** Compiling for UNIT TEST ***'
+	@echo -e '*******************************'
+	@mkdir -p build
+	@gcc main.c sbuffer.c   -lmylist -L/lib/ -g -o build/test $(FLAGS)
+	@echo -e '\n*************************'
+	@echo -e '*** Running UNIT TEST ***'
+	@echo -e '*************************'
+	@gdb -q -tui ./build/test
+valgrind: main.c sbuffer.c
+	@echo -e '\n*******************************'
+	@echo -e '*** Compiling for UNIT TEST ***'
+	@echo -e '*******************************'
+	@mkdir -p build
+	@gcc main.c sbuffer.c -lmylist -L/lib/ -g -o build/test $(FLAGS)
+	@echo -e '*** Running UNIT TEST ***'
+	@echo -e '*************************'
+	@export CK_FORK=no ;  valgrind -s ./build/test
+zip:
+	zip lab7_ex2.zip connmgr.c  errmacros.h connmgr.h config.h lib/dplist.c lib/dplist.h lib/tcpsock.c lib/tcpsock.h
---
+++ source/client_cert.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#define MAXBUF 1024
+
+void ShowCerts(SSL * ssl)
+{
+    X509 *cert;
+    char *line;
+
+    cert = SSL_get_peer_certificate(ssl);
+    // SSL_get_verify_result()This is the point. SSL_CTX_set_verify()Only when the configuration is enabled or not and the authentication is not executed, can the function be called to verify the authentication
+    // If the verification fails, the program throws an exception to terminate the connection
+    if(SSL_get_verify_result(ssl) == X509_V_OK){
+        printf("Certificate verification passed\n");
+    }
+    if (cert != NULL) {
+        printf("Digital certificate information:\n");
+        line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+        printf("certificate: %s\n", line);
+        free(line);
+        line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+        printf("Issuer: %s\n", line);
+        free(line);
+        X509_free(cert);
+    } else
+        printf("No certificate information!\n");
+}
+
+int main(int argc, char **argv)
+{
+    int sockfd, len;
+    struct sockaddr_in dest;
+    char buffer[MAXBUF + 1];
+    SSL_CTX *ctx;
+    SSL *ssl;
+
+    if (argc != 5) {
+        printf("Parameter format error! The correct usage is as follows:\n\t\t%s IP Address port\n\t such as:\t%s 127.0.0.1 80\n This program is used to"
+             "IP The server with the address receives the most from one port MAXBUF Bytes of messages",
+             argv[0], argv[0]);
+        exit(0);
+    }
+
+    /* SSL For library initialization, refer to ssl-server.c code */
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+    ctx = SSL_CTX_new(SSLv23_client_method());
+    if (ctx == NULL) {
+        ERR_print_errors_fp(stdout);
+        exit(1);
+    }
+
+    // Two way validation
+    // SSL_VERIFY_PEER---Certificate certification is required and will be released without certificate
+    // SSL_VERIFY_FAIL_IF_NO_PEER_CERT---The client is required to provide a certificate, but it will be released if no certificate is used alone
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER|SSL_VERIFY_FAIL_IF_NO_PEER_CERT, NULL);
+    // Set trust root certificate
+    if (SSL_CTX_load_verify_locations(ctx, "keys/ca.crt",NULL)<=0){
+        ERR_print_errors_fp(stdout);
+        exit(1);
+    }
+
+    /* Loads the user's digital certificate, which is used to send to the client. Certificate contains public key */
+    if (SSL_CTX_use_certificate_file(ctx, argv[3], SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stdout);
+        exit(1);
+    }
+    /* Load user private key */
+    if (SSL_CTX_use_PrivateKey_file(ctx, argv[4], SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stdout);
+        exit(1);
+    }
+    /* Check if the user's private key is correct */
+    if (!SSL_CTX_check_private_key(ctx)) {
+        ERR_print_errors_fp(stdout);
+        exit(1);
+    }
+
+    /* Create a socket for tcp communication */
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket");
+        exit(errno);
+    }
+    printf("socket created\n");
+
+    /* Initialize the address and port information of the server (opposite party) */
+    //bzero(&dest, sizeof(dest));
+    memset(&dest, 0,sizeof(dest));
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(atoi(argv[2]));
+    if (inet_aton(argv[1], (struct in_addr *) &dest.sin_addr.s_addr) == 0) {
+        perror(argv[1]);
+        exit(errno);
+    }
+    printf("address created\n");
+
+    /* Connect to server */
+    if (connect(sockfd, (struct sockaddr *) &dest, sizeof(dest)) != 0) {
+        perror("Connect ");
+        exit(errno);
+    }
+    printf("server connected\n");
+
+    /* A new SSL based on ctx */
+    ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, sockfd);
+    /* Establish SSL connection */
+    if (SSL_connect(ssl) == -1)
+        ERR_print_errors_fp(stderr);
+    else {
+        printf("Connected with %s encryption\n", SSL_get_cipher(ssl));
+        ShowCerts(ssl);
+    }
+
+    /* Receive messages sent by the other party, Max buf bytes at most */
+    //bzero(buffer, MAXBUF + 1);
+    memset(&buffer, 0,MAXBUF + 1);
+    /* Receive message from server */
+    len = SSL_read(ssl, buffer, MAXBUF);
+    if (len > 0)
+        printf("Message received successfully:'%s'，common%d Bytes of data\n",
+               buffer, len);
+    else {
+        printf
+            ("Message reception failed! The error code is%d，The error message is'%s'\n",
+             errno, strerror(errno));
+        goto finish;
+    }
+    //bzero(buffer, MAXBUF + 1);
+    memset(&buffer, 0,MAXBUF + 1);
+    strcpy(buffer, "from client->server");
+    /* Send message to server */
+    len = SSL_write(ssl, buffer, strlen(buffer));
+    if (len < 0)
+        printf
+            ("news'%s'Sending failed! The error code is%d，The error message is'%s'\n",
+             buffer, errno, strerror(errno));
+    else
+        printf("news'%s'Sent successfully, sent in total%d Byte!\n",
+               buffer, len);
+
+  finish:
+    /* Close connection */
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    close(sockfd);
+    SSL_CTX_free(ctx);
+    return 0;
+}
