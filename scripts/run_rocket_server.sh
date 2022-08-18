6fa8b28e9c361471402b0f0324fd6b9c10f37fbb
patricioadolfo.castillocalderon <you@example.com>
2022-08-17 23:43:10 -0500

    script_for_server_added EDITED

--- scripts/run_rocket_server.sh
+++ scripts/run_rocket_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo ../build/rocket_server  85354 keys/server.crt keys/server_rsa_private.pem.unsecure
+sudo /home/debian/wireless_rocket_server/build/rocket_server  85354 ../keys/server.crt ../keys/server_rsa_private.pem.unsecure
