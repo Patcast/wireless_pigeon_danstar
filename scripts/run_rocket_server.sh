0101c0702e3001309010d613bf48e5b111da2633
patricioadolfo.castillocalderon <you@example.com>
2022-08-17 23:46:14 -0500

    adding absolute paths to files

--- scripts/run_rocket_server.sh
+++ scripts/run_rocket_server.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sudo /home/debian/wireless_rocket_server/build/rocket_server  85354 ../keys/server.crt ../keys/server_rsa_private.pem.unsecure
+export PATH=/home/debian/wireless_rocket_server
+sudo $PATH/build/rocket_server  85354 $PATH/keys/server.crt $PATH/keys/server_rsa_private.pem.unsecure
