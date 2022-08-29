8e1dba1f1f2ee9fc2f9162f3b351aa2929304695
patricioadolfo.castillocalderon <you@example.com>
2022-08-29 13:45:37 -0500

    executable script for client

--- scripts/run_client.sh
+++ scripts/run_client.sh
@@ -1,1 +1,7 @@
 /home/debian/ignition_box/build/ignition_box  192.168.192.218 85354 /home/debian/ignition_box/keys/client.crt /home/debian/ignition_box/keys/client_rsa_private.pem.unsecure /home/debian/ignition_box/keys/ca.crt 
+
+echo "\n\n\nProgram is closed, rebooting device...\n\n\n"
+
+sudo reboot 
+
+# chmod +x <fileName> to run it as an executable
