96ec5c13731bfe59e87d3dba65c1e49a77dd3994
patricioadolfo.castillocalderon <you@example.com>
2022-08-29 13:09:06 -0500

    adding reboot with an script

--- scripts/run_rocket_server.sh
+++ scripts/run_rocket_server.sh
@@ -2,3 +2,7 @@
 
 
 /home/debian/rocket_server/build/rocket_server  85354 /home/debian/rocket_server/keys/server.crt /home/debian/rocket_server/keys/server_rsa_private.pem.unsecure /home/debian/rocket_server/keys/ca.crt
+
+echo "Program is closed, rebooting device..."
+
+reboot 
