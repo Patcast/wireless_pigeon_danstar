ccf422c9236beb1c777a89c12f014b664b70f696
patricioadolfo.castillocalderon <you@example.com>
2022-08-29 13:39:05 -0500

    force sudo on reboot

--- scripts/run_rocket_server.sh
+++ scripts/run_rocket_server.sh
@@ -3,6 +3,8 @@
 
 /home/debian/rocket_server/build/rocket_server  85354 /home/debian/rocket_server/keys/server.crt /home/debian/rocket_server/keys/server_rsa_private.pem.unsecure /home/debian/rocket_server/keys/ca.crt
 
-echo "Program is closed, rebooting device..."
+echo "\n\n\nProgram is closed, rebooting device...\n\n\n"
 
-reboot 
+sudo reboot 
+
+# chmod +x <fileName> to run it as an executable
