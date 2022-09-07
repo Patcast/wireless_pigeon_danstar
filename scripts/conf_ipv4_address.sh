c9b6bf1075503c56484b688c271263fc7d015b3f
patricioadolfo.castillocalderon <you@example.com>
2022-09-07 11:41:53 -0500

    fixed conf. ip4 script

--- scripts/conf_ipv4_address.sh
+++ scripts/conf_ipv4_address.sh
@@ -6,5 +6,5 @@ sudo ip addr flush dev $lan_name
 
 echo "Type the new ipV4 address for the LAN"
 read lan_addr
-sudo ip addr add $lan_addr /27 dev $lan_name
+sudo ip addr add $lan_addr/27 dev $lan_name
 echo "Address successfully changed "
